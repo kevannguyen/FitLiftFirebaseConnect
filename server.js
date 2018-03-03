e3619415f19dd5558255e6eca3521ad91536db68
Kevan Nguyen <kevanhn@uci.edu>
2018-03-02 21:39:29 -0800

    Setup API to sign into Fitlift Firebase

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var https = require('https');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+    response.header("Content-Type",'application/json');
+    response.send('{"Error": "POST requests only!"}');
+});
+
+
+app.post('/', function(request, response) {
+    response.header("Content-Type",'application/json');
+    var e = request.body.email;
+    var p = request.body.password;
+
+    var postData = '{"email":"' + e + '",password:"' + p + '","returnSecureToken":true}';
+    var options = {
+        hostname: 'www.googleapis.com',
+        port: 443,
+        path: '/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAOFwsIxlQSaONcrlNFKRKDp5W-Ug_QSuY',
+        method: 'POST',
+        headers: {
+             'Content-Type': 'application/json'
+           }
+    };
+
+    var req = https.request(options, (res) => {    
+        var str = '';
+        res.on('data', (d) => {
+            str += d;
+        });
+        res.on('end', function () {
+            response.send(str);
+        });
+    });
+    
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    
+    req.write(postData);
+    req.end();
+});
+
+app.listen(3000);
